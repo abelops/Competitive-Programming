12e9dc5a67626b412438cfed1a91ff71c4bfecdc
Abel Getahun <88226208+abelops@users.noreply.github.com>
2022-10-23 14:32:10 +0300

    Time: 5927 ms (6.12%), Space: 15.7 MB (60.69%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,21 +1,33 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        nums.sort()
         ans = []
         miss = []
         mp = {}
         mis=0
+        fin = []
+        prev = None
         for i in range(len(nums)):
             if i+1 not in nums:
                 mis = i+1
-                miss.append(mis)
+                if prev == "rep":
+                    fin.append(mis)
+                    prev = "nf"
+                else:
+                    miss.append(mis)
             if nums[i] not in mp:
                 mp[nums[i]] = i
             else:
-                ans.append(nums[i])
+                if prev == None or prev == "nf":
+                    fin.append(nums[i])
+                    prev = "rep"
+                    if miss:
+                        an = miss.pop()
+                        fin.append(an)
+                        prev = "nf"
+                else:
+                    fin.append(miss[-1])
+                    ans.append(nums[i])
+                    prev = "rep"
+                fin
                 mis=0
-        fin = []
-        for i in range(len(ans)):
-            fin.append(ans[i])
-            fin.append(miss[i])
         return fin
