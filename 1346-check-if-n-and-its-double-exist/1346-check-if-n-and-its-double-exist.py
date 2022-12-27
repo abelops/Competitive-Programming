a79461d4c3abb970af5423acf441e38cdd0b4064
Abel Getahun <88226208+abelops@users.noreply.github.com>
2022-12-27 09:12:30 +0300

    Time: 123 ms (34.25%), Space: 14 MB (15.40%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -1,30 +1,19 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        arr.sort()
-        l = 0
-        r = 1
-        s = len(arr)
-        c = arr.count(0)
-        # while l < s:
-        #     # print(l,r)
-        #     # print(arr)
-        #     if c > 1:
-        #         return True
-        #     if arr[l] * 2 == arr[r]:
-        #         print(arr[l], arr[r])
-        #         return True
-        #     if arr[l] * 2 < arr[r] and l < r:
-        #         l+=1
-        #     elif arr[l] * 2 > arr[r] and r < s-1:
-        #         r+=1
-        #     else:
-        #         l+=1
-        # return False
-        for i in range(len(arr)):
-            for j in range(len(arr)):
-                if arr[i] * 2 == arr[j] and i != j:
+        mp = {}
+        for i, num in enumerate(arr):
+            if i not in arr:
+                mp[num] = i
+        for i, num in enumerate(arr):
+            if num*2 in mp:
+                if mp[num*2] != i:
                     return True
         return False
+#         for i in range(len(arr)):
+#             for j in range(len(arr)):
+#                 if arr[i] * 2 == arr[j] and i != j:
+#                     return True
+#         return False
             
             
         
