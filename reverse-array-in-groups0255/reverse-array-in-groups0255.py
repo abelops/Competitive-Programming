97ae7f77db6ff02905f86a927cc36e6f7046be60
abelops <abelgetahun55@gmail.com>
2022-12-29 09:52:08 +0300

    more solution

--- reverse-array-in-groups0255/reverse-array-in-groups0255.py
+++
@@ -1,9 +0,0 @@
-#User function template for Python
-
-class Solution:	
-    #Function to reverse every sub-array group of size k.
-	def reverseInGroups(self, arr, N, K):
-		# code here
-		c=0
-		for i in range(0,N,K):
-		    arr[i:i+K] = arr[i:i+K][::-1]
