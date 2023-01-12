def8e6e641035c714977262471709ad791a104cb
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-01-12 07:15:28 +0300

    Update 0283-move-zeroes.py

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,21 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l=0
-        r=1
-        while(r<len(nums) and 0 in nums):
-            if(nums[l]!=0):
+        leng = len(nums)
+        l = 0
+        r = 1
+        
+        while r < len(nums):
+            if nums[r] == 0:
+                r+=1
+                continue
+            if l == r:
+                r+=1
+                continue
+            if nums[l] == 0:
+                nums[l] = nums[r]
+                nums[r] = 0
                 l+=1
                 r+=1
-            elif(nums[l]==0 and nums[r]==0):
-                r+=1
-            elif(nums[l]==0 and nums[r]!=0):
-                nums[l]=nums[r]
-                nums[r]=0
-                if(r==l):
-                    r+=1
-                l+=1
             else:
-                r+=1
+                l+=1
                 
-        return nums
+        
