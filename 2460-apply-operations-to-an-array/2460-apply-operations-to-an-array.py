9ac2727a9bdec5039c9df7870ec73e1e8cc4a14e
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-01-12 07:13:27 +0300

    Update 2460-apply-operations-to-an-array.py

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -5,12 +5,23 @@ class Solution:
                 if nums[i] == nums[i+1]:
                     nums[i] = nums[i] * 2
                     nums[i+1] = 0
-        ans = []
-        z = []
-        for i in nums:
-            if i != 0:
-                ans.append(i)
+        l = 0
+        r = 1
+        while r < len(nums):
+            if nums[r] == 0:
+                r+=1
+                continue
+            if l == r:
+                r+=1
+                continue
+            if nums[l] == 0:
+                nums[l] = nums[r]
+                nums[r] = 0
+                if l+1 < len(nums):
+                    l+=1
+                elif r+1 < len(nums):
+                    r+=1
             else:
-                z.append(0)
-        ans += z
-        return ans
+                l+=1
+                    
+        return nums
