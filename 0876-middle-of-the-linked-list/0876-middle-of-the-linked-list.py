3784c731667d9f0ac5b0781c4229bd95a60ea0ef
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-02-20 20:08:00 +0300

    Update 0876-middle-of-the-linked-list.py

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -5,20 +5,11 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        arr = []
-        while(head!=None):
-            arr.append(head.val)
-            head=head.next
-        li = ListNode(0)
-        head = li
-        if(len(arr)%2==0):
-            n=int(len(arr)/2)
-            res = arr[n:len(arr)]
-        else:
-            res = arr[int(len(arr)/2):len(arr)]
-        for i in range(len(res)):
-            no = ListNode(res[i])
-            li.next = no
-            li=li.next
-        return head.next
-        
+        fast = head.next
+        slow = head
+        if not fast:
+            return head
+        while fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow.next
