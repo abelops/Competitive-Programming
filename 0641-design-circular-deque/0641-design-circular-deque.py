f9b35fcfda86a0a192803afd8b596cf745595a92
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-02-28 16:18:09 +0300

    Time: 62 ms (98.16%), Space: 14.7 MB (50.82%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.py
+++ 0641-design-circular-deque/0641-design-circular-deque.py
@@ -2,70 +2,63 @@ class MyCircularDeque:
 
     def __init__(self, k: int):
         self.k = k
-        self.q = deque()
-        self.size = 0
+        self.stack = []
+
     def insertFront(self, value: int) -> bool:
-        if not self.isFull():
-            self.q.appendleft(value) 
-            self.size += 1
+        if(len(self.stack)<self.k):
+            if(len(self.stack)==0):
+                self.stack.append(value)
+            else:
+                temp = self.stack
+                self.stack = []
+                self.stack.append(value)
+                self.stack = self.stack + temp
             return True
         else:
             return False
-
     def insertLast(self, value: int) -> bool:
-        if not self.isFull():
-            self.q.append(value)
-            self.size += 1
+        if(len(self.stack)<self.k):
+            self.stack.append(value)
             return True
         else:
             return False
 
     def deleteFront(self) -> bool:
-        if not self.isEmpty():
-            self.q.popleft()
-            self.size -= 1
+        if(len(self.stack)>0):
+            self.stack.reverse()
+            self.stack.pop()
+            self.stack.reverse()
             return True
         else:
             return False
 
     def deleteLast(self) -> bool:
-        if not self.isEmpty():
-            self.q.pop()
-            self.size -= 1
+        if(len(self.stack)>0):
+            self.stack.pop()
             return True
         else:
             return False
 
     def getFront(self) -> int:
-        if not self.isEmpty():
-            return self.q[0]
+        if(len(self.stack)>0):
+            return self.stack[0]
         else:
             return -1
+
     def getRear(self) -> int:
-        if not self.isEmpty():
-            return self.q[-1]
+        if(len(self.stack)>0):
+            return self.stack[-1]
         else:
             return -1
+
     def isEmpty(self) -> bool:
-        if self.size == 0:
+        if(len(self.stack)==0):
             return True
         else:
             return False
 
     def isFull(self) -> bool:
-        if self.size == self.k:
+        if(len(self.stack)==self.k):
             return True
         else:
             return False
-
-
-# Your MyCircularDeque object will be instantiated and called as such:
-# obj = MyCircularDeque(k)
-# param_1 = obj.insertFront(value)
-# param_2 = obj.insertLast(value)
-# param_3 = obj.deleteFront()
-# param_4 = obj.deleteLast()
-# param_5 = obj.getFront()
-# param_6 = obj.getRear()
-# param_7 = obj.isEmpty()
-# param_8 = obj.isFull()
