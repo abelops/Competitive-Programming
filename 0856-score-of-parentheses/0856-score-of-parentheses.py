423cd78c05b525755d38ac1b541ac38a16d0ba22
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-03-01 15:37:32 +0300

    Time: 42 ms (11.81%), Space: 13.9 MB (50.74%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.py
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -1,30 +1,16 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-        st = []
+        st = [0]
         for i in s:
-            if i == "(":
-                st.append(i)
-            elif i == ")":
-                if st[-1] == "(":
-                    st.pop()
-                    if st:
-                        if st[-1] != "(" and st[-1] != ")":
-                            temp = st.pop() + 1
-                            st.append(temp)
-                        else:
-                            st.append(1)
-                    else:
-                        st.append(1)
+            if i == ")":
+                temp = st.pop()
+                if temp == 0:
+                    st[-1] += 1
                 else:
-                    temp = st.pop()
-                    st.pop()
-                    if st:
-                        if st[-1] != "(" and st[-1] != ")":
-                            temp1 = st.pop()
-                            st.append(temp1 + temp*2)
-                        else:
-                            st.append(temp * 2)
-                    else:
-                        st.append(temp * 2)
-            # print(st)
-        return st[-1]
+                    st[-1] += 2 * temp
+            else:
+                st.append(0)
+        return st[0]
+                
+    
+            
