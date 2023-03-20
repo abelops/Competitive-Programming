6ae65d3aee9d3ec55f9248441f46c74e1228621f
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-03-20 17:57:14 +0300

    Time: 46 ms (9.94%), Space: 14.1 MB (78.01%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         solutions = []
-        def backtrack(start,state):
-            if state in solutions:
+        def backtrack(start, cur):
+            nonlocal solutions
+            if cur not in solutions:
+                solutions.append(cur[:])
+            if len(cur) == len(nums):
                 return
-            else:
-                solutions.append(state[:])
-            for i in range(start,len(nums)):
-                state.append(nums[i])
-                backtrack(i+1,state)
-                state.pop()
+                
+            for i in range(start, len(nums)):
+                cur.append(nums[i])
+                backtrack(i+1, cur)
+                cur.pop()
         backtrack(0, [])
+        
         return solutions
+    
+    
