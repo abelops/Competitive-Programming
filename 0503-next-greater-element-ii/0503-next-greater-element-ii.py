a6b2da61171f678530b00491c18e867f7c3006d2
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-03-21 11:55:11 +0300

    Time: 213 ms (75.64%), Space: 15.7 MB (36.87%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,20 +1,13 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         st = []
+        lens = len(nums[:])
         nums.extend(nums)
-        mp = defaultdict(int)
-        dis = []
+        ans = [-1] * (len(nums))
         for i in range(len(nums)):
-            if st:
-                while st and nums[st[-1]] < nums[i]:
-                    temp = st.pop()
-                    mp[temp] = nums[i]
+            while st and nums[st[-1]] < nums[i]:
+                temp = st.pop()
+                ans[temp] = nums[i]
             st.append(i)
-        st = []
-        for i in range(len(nums)):
-            if i not in mp:
-                mp[i] = -1
-        ans = []
-        for i in range(len(nums)):
-            ans.append(mp[i])
-        return ans[:int(len(nums)/2)]
+        return ans[:lens]
+        
