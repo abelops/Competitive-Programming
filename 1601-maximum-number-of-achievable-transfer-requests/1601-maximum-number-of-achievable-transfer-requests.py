7729f3c253ae4b814dfaaa8be0ae5c4d853b7a63
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-03-27 08:08:14 +0300

    Time: 4070 ms (20.59%), Space: 14 MB (38.24%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
@@ -1,17 +1,13 @@
 class Solution:
     def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
-        ans = []
+        # ans = []
         c = 0
         def helper(arr):
-            leave = defaultdict(int)
-            join = defaultdict(int)
+            leave = {}
+            join = {}
             for i in arr:
-                if i[0] == i[1]:
-                    continue
-                leave[i[0]]+=1
-                join[i[1]]+=1
-            # if join == leave:
-            #     print(arr)
+                leave[i[0]] = leave.get(i[0], 0) + 1
+                join[i[1]] = join.get(i[1], 0) + 1
             return join == leave
         
         def comb(start, cur):
@@ -19,16 +15,10 @@ class Solution:
             if cur:
                 if helper(cur[:]):
                     c = max(c, len(cur))
-                ans.append(cur[:])
             
             for i in range(start, len(requests)):
-                # print(i)
                 cur.append(requests[i])
                 comb(i+1, cur)
                 cur.pop()
         comb(0, [])
-        # for i in ans:
-        #     if len(i) == 8:
-        #         print(i, sum(i[0]), sum(i[1]))
-        # print(len(ans))
         return c
