45e255e31cd4304b376f6f93dbabacd4764e3a80
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-03-29 21:16:30 +0300

    Time: 94 ms (47.35%), Space: 20.9 MB (19.07%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,5 +1,16 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        def dp():
+            ans= [0,1]
+            if n == 0:
+                return [0]
+            for i in range(n-1):
+                if i % 2 == 0:
+                    ans.append(ans[i//2+1])
+                else:
+                    ans.append(ans[i//2+1]+1)
+            return ans
+                
         def shiftNum():
             ans = []
             for i in range(n+1):
@@ -22,6 +33,6 @@ class Solution:
                     one = one << 1
                 ans.append(c)
             return ans
-        return shiftNum()
+        return dp()
             
             
