f188dea85bdb8088722869ca7ee3efa1895f2ae2
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-03-31 19:07:39 +0300

    Time: 40 ms (9.79%), Space: 14 MB (39.83%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,5 +1,4 @@
 class Solution:
     def findComplement(self, num: int) -> int:
         bitLen = num.bit_length()
-        one = (1 << bitLen) -1
-        return num ^ one
+        return num ^ ((1 << bitLen) -1)
