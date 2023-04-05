f9199319bb34cb7b64ba0538265939e2e8371e4d
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-04-05 19:11:34 +0300

    Time: 35 ms (72.89%), Space: 13.8 MB (65.06%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,16 +1,14 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        def factorize(n):
-            ans= []
-            i = 2
-            while i < n: 
-                while n % i == 0:
-                    ans.append(int(i))
-                    n /= i
-                i+=1
-            if n != 1:
-                ans.append(int(n))
-            return ans
-        return sum(factorize(n))
+        ans= 0
+        i = 2
+        while i < n: 
+            while n % i == 0:
+                ans+=int(i)
+                n /= i
+            i+=1
+        if n != 1:
+            ans+=int(n)
+        return ans
         
                 
