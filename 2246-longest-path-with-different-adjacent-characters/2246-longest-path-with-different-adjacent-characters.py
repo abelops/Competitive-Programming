0508953ff7ff38581b4ce7c01f0959fb1e525acc
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-04-15 19:37:44 +0300

    Time: 2086 ms (33.45%), Space: 159.4 MB (27.11%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -7,29 +7,29 @@ class Solution:
                 mp[i] = []
             if parent[i] != -1:
                 mp[parent[i]].append(i)
-        # print(mp)
+                
         def dfs(num):
             nonlocal ans
             if not mp[num]:
                 return [s[num], 1]
             arr = []
+            c = 0
             for i in mp[num]:
                 ret = dfs(i)
                 if ret and ret[0] != s[num]:
                     ret[0] = s[num]
                     ret[1]+=1
+                    c+=1
                     arr.append(ret)
             arr = sorted(arr, key=lambda x: x[1])
-            # print(num,arr)
-            if len(arr) > 1:
+            if c > 1:
                 ans = max(ans, arr[-2][1] + arr[-1][1] -1)
-            elif len(arr) == 1:
+            elif c == 1:
                 ans = max(ans, arr[0][1])
             if not arr:
                 return [s[num], 1]
             return arr[-1]
         dfs(0)
-        # print(ans)
         return ans
             
             
