d1c9d42fc8a6157a598466c093c6a85bdb1036c8
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-04-23 16:51:42 +0300

    Time: 185 ms (75.91%), Space: 14.9 MB (19.28%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,29 +1,18 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        mp = defaultdict(list)
+        def dfs(city, visited):
+            visited.add(city)
+            
+            for i in range(len(isConnected)):
+                if isConnected[city][i] and i!=city and i not in visited:
+                    dfs(i, visited)
+        visited = set()
+        ans = 0
         for i in range(len(isConnected)):
-            for j in range(len(isConnected[i])):
-                if isConnected[i][j] == 1:
-                    mp[i+1].append(j+1)
-        # print(mp)
-        
-        provinces = []
-        visited = []  
-        for i in range(len(isConnected)):
-            st = [i+1]
-            ans = []
-            while st:
-                poped = st.pop()
-                if poped not in visited:
-                    ans.append(poped)
-                    visited.append(poped)
-                for i in mp[poped]:
-                    if i != poped and i not in visited:
-                        st.append(i)
-                # print(visited)
-            if ans:
-                provinces.append(ans)
-        # print(provinces)
-        return len(provinces)
+            if i not in visited:
+                dfs(i, visited)
+                ans+=1
+        return ans
+            
             
             
