ccfaf445415cc1fbc3cf9f1afa7e88e73955518f
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-05-30 17:22:34 +0300

    Time: 1669 ms (23.67%), Space: 47.4 MB (5.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,26 +1,21 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        q = deque([amount])
-        ans = float("inf")
-        level = 0
-        if amount == 0:
-            return 0
-        visited = set()
-        while q:
-            leng = len(q)
-            for i in range(leng):
-                poped = q.popleft()
-                for j in coins:
-                    diff = poped - j
-                    if diff not in visited:
-                        if diff == 0:
-                            ans = min(ans, level+1)
-                            # print(level, ans)
-                            break
-                        if diff > 0:
-                            q.append(diff)
-                        visited.add(diff)
-            level+=1
-        # print(ans)
-        
-        return ans if ans != float("inf") else -1
+        memo = {}
+        if len(coins) == 1 and amount % coins[0] != 0:
+            return -1
+        def backtrack(cur):
+            if cur == 0:
+                return 0
+            if cur in memo:
+                return memo[cur]
+            minn = float("inf")
+            for j in coins:
+                if cur - j >= 0:
+                    temp = backtrack(cur-j)
+                    minn = min(minn, temp)
+            memo[cur] = minn + 1
+            return minn + 1
+        ans = backtrack(amount)
+        return ans if ans != float("inf") else -1 
+            
+            
