d39a101766bf57b982e2bd894b393b9db8b4b9d6
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-06-04 13:04:01 +0300

    Time: 82 ms (40.44%), Space: 17.4 MB (29.01%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,23 +1,7 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         leng = len(triangle)
-        if leng == 1:
-            return triangle[0][0]
-        minn = float("inf")
-        def dp(cur, level, memo):
-            # print(cur, level)
-            if level == leng-1:
-                return triangle[-1][cur]
-            
-            if (cur, level) in memo:
-                return memo[(cur, level)]
-            ans = float("inf")
-            for i in range(cur, level+1):
-                left = dp(cur+1, level+1, memo) 
-                right = dp(cur, level+1, memo) 
-                ans = min(left, right)
-            ans += triangle[level][cur]
-            memo[(cur, level)] = ans
-            return ans
-                
-        return dp(0, 0,{})
+        for i in range(leng-2, -1, -1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] = triangle[i][j] + min(triangle[i+1][j],triangle[i+1][j+1])
+        return triangle[0][0]
