0a696d9b067308f577fc7a07dafd7279575bfed5
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-06-04 16:48:58 +0300

    Time: 41 ms (58.84%), Space: 16.2 MB (47.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,18 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = {}
-        def recur(n, amount):
-            # print(n, amount)
-            if n >= len(nums):
-                return amount
-            if (n, amount) in memo:
-                return memo[(n, amount)]
-            left = recur(n+2, amount + nums[n])
-            right = recur(n+1, amount)
-            maxi = max(left, right)
-            memo[(n, amount)] = maxi
-            return maxi
-        return recur(0, 0)
+        leng = len(nums)
+        if leng < 3:
+            return max(nums)
+        ans = [0] * leng
+        ans[0], ans[1] = nums[0], max(nums[1], nums[0])
+        for i in range(2, leng):
+            ans[i] = max(ans[i-1], ans[i-2] + nums[i])
+        return ans[-1]
             
                 
             
