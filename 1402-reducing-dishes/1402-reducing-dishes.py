76ce284b6f86c5c1d625027e1a28e14aac825e2f
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-06-05 19:33:12 +0300

    Time: 606 ms (11.57%), Space: 157.1 MB (5.14%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -3,10 +3,14 @@ class Solution:
         sat = sorted(satisfaction)
         leng = len(satisfaction)
         maxi = -float("inf")
+        memo = {}
         def dfs(tot, cur, m):
             if cur == leng:
                 return tot
-            return dfs(tot + (sat[cur] * m), cur+1, m+1) 
+            if (cur, m) in memo:
+                return memo[(cur, m)]
+            memo[(cur, m)] = dfs(tot + (sat[cur] * m), cur+1, m+1) 
+            return memo[(cur, m)]
             
         for i in range(leng-1, -1, -1):
             ret = dfs(0, i, 1)
