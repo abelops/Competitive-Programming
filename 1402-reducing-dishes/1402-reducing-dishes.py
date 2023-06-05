b47c28e63f7f004f4b6246acb93b43f13101f18a
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-06-05 19:24:17 +0300

    Time: 270 ms (15.28%), Space: 17.1 MB (13.14%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        sat = sorted(satisfaction)
+        leng = len(satisfaction)
+        maxi = -float("inf")
+        def dfs(tot, cur, m):
+            if cur == leng:
+                return tot
+            return dfs(tot + (sat[cur] * m), cur+1, m+1) 
+            
+        for i in range(leng-1, -1, -1):
+            ret = dfs(0, i, 1)
+            if ret <= maxi:
+                break
+            maxi = max(maxi, ret)
+        return maxi if maxi > 0 else 0
