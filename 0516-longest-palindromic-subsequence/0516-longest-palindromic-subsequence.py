b64fdfd35c499492ae9fa668d850f2b2f210e54d
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-09-29 14:25:15 +0300

    Time: 2018 ms (20.75%), Space: 543 MB (5.00%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,14 +1,34 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
+        memo = {}
+        l = len(s)
         text1 = s
         text2 = s[::-1]
-        len1 = len2 = len(s)
-        mat = [[0] * (len2+1) for x in range(len1+1)]
-        for i in range(len1-1,-1,-1):
-            for j in range(len2-1, -1, -1):
+        def dp(i,j):
+            if (i,j) in memo:
+                return memo[(i,j)]
+            if i < l and j < l:
                 if text1[i] == text2[j]:
-                    mat[i][j]= 1 + mat[i+1][j+1]  
+                    temp = dp(i+1, j+1)
+                    memo[(i,j)] = temp + 1
+                    
                 else:
-                    mat[i][j]= max(mat[i+1][j], mat[i][j+1])
-        return mat[0][0]
+                    left = dp(i+1, j)
+                    right = dp(i, j+1)
+                    memo[(i,j)] = max(left, right)
+                return memo[(i,j)]
+            return 0
+        return dp(0,0)
+            
+        # text1 = s
+        # text2 = s[::-1]
+        # len1 = len2 = len(s)
+        # mat = [[0] * (len2+1) for x in range(len1+1)]
+        # for i in range(len1-1,-1,-1):
+        #     for j in range(len2-1, -1, -1):
+        #         if text1[i] == text2[j]:
+        #             mat[i][j]= 1 + mat[i+1][j+1]  
+        #         else:
+        #             mat[i][j]= max(mat[i+1][j], mat[i][j+1])
+        # return mat[0][0]
         
