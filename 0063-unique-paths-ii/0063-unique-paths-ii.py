f4325b75ea7bc5368b06fff4dc7d30c256713f83
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-09-30 21:45:37 +0300

    Time: 49 ms (54.28%), Space: 16.3 MB (76.65%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,22 +1,29 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        r, c = len(obstacleGrid), len(obstacleGrid[0])
-        mat = [[0 for i in range(c)] for k in range(r)]
-        mat[0][0] = 1
-        d = [(-1,0), (0,-1)]
-        if obstacleGrid[0][0]:
+        if obstacleGrid[0][0] == 1 or obstacleGrid[-1][-1] == 1:
             return 0
-        def inBound(i,j):
-            return 0 <= i < r and 0 <= j < c
-        
-        for i in range(r):
-            for j in range(c):
-                if not obstacleGrid[i][j]:
-                    for x, y in d:
-                        dx = i + x
-                        dy = j + y
-                        if inBound(dx, dy):
-                            # print(dx, dy, i, j, r, c, mat)
-                            mat[i][j]+= mat[dx][dy]
-        return mat[r-1][c-1]
-                            
+        row, col = len(obstacleGrid), len(obstacleGrid[0])
+        for i in range(row):
+            for j in range(col):
+                if obstacleGrid[i][j] == 1:
+                    obstacleGrid[i][j] = -1
+                    
+        for i in range(row):
+            for j in range(col):
+                if obstacleGrid[i][j] != -1:
+                    if i == 0 and j > 0 and obstacleGrid[i][j-1] == -1:
+                        obstacleGrid[i][j] = -1
+                    elif i == 0:
+                        obstacleGrid[i][j] = 1
+                    if j == 0 and i > 0 and obstacleGrid[i-1][j] == -1:
+                        obstacleGrid[i][j] = -1
+                    elif j == 0:
+                        obstacleGrid[i][j] = 1
+                if i > 0 and j > 0 and obstacleGrid[i][j] != -1:
+                    top = obstacleGrid[i-1][j] if obstacleGrid[i-1][j] != -1 else 0
+                    left = obstacleGrid[i][j-1] if obstacleGrid[i][j-1] != -1 else 0
+                    obstacleGrid[i][j] = top+left
+        # print(obstacleGrid)
+        return obstacleGrid[-1][-1] if obstacleGrid[-1][-1] > 0 else 0
+    
+    
