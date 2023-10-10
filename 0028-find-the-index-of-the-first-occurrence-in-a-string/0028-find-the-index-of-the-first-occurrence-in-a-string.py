a7b5117bad05cf50a6f1d18d1b60a1de743ddb1a
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-10-10 16:36:09 +0300

    Time: 42 ms (28.40%), Space: 16.1 MB (96.64%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,10 +1,32 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        l, r = 0, 0
-        stat = -1
-        flen = len(haystack)
-        slen = len(needle)
-        for i in range(flen):
-            if haystack[i: i+slen] == needle:
-                return i
+        if len(needle) > len(haystack):
+            return -1
+        search = 0
+        l = len(needle)
+        for i in range(l):
+            search+=(ord(needle[i])-ord('a')+1)*(26**(l-i-1)) 
+        # search %= 10 ** 9 + 7
+        cur = 0 
+        for i in range(l):
+            cur += (ord(haystack[i])-ord('a')+1)*(26**(l-i-1))
+
+        if cur  == search:
+            return 0
+        
+        
+        for i in range(l, len(haystack)):
+            # if cur == search:
+            #     return i
+            
+            cur -= (26 ** (len(needle) - 1) * (ord(haystack[i - len(needle)]) - ord('a') + 1)) 
+            
+            # cur-= (ord(haystack[i-l-1]) - ord('a')+1) * (26**(l-1))% (10 ** 9 + 7)
+            cur*= 26
+            cur+= ord(haystack[i]) - ord('a') +1
+            # print(cur, search)
+            if cur == search:
+                return i-l+1
+
         return -1
+            
