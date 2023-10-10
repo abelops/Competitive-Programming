fd3fc72e36536ab70eabf02c929e1ed89347c0e7
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-10-10 23:06:26 +0300

    Time: 4835 ms (6.15%), Space: 16.8 MB (44.62%) - LeetHub

--- 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.py
+++ 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.py
@@ -1,28 +1,23 @@
 class Solution:
     def subStrHash(self, s: str, power: int, modulo: int, k: int, hashValue: int) -> str:
-        memo = {}
-        powers = [1] * len(s)
-        for i in range(1, len(s)):
-            powers[i] = powers[i-1] * power
-            
-        def hasher(start):
-            cur = 0
-            for i in range(k):
-                cur += (ord(s[i]) - 97+1) * powers[i]
-            return cur
-        def adder(prev, i):
-            prev -= (ord(s[i-k]) - 97+1)
-            prev//=power
-            prev += (ord(s[i]) - 97 +1) * powers[k-1]
-            return prev
-        cur = hasher(0)
-        if cur%modulo == hashValue:
-            return s[:k]
-            
-        for i in range(k,len(s)):
-            cur = adder(cur, i)
-            if cur%modulo == hashValue:
-                # print(cur)
-                return s[i-k+1:i+1]
+        l = len(s)
+        cur = 0
+        prev = 1
+        for i in range(l-k, l):
+            cur += (ord(s[i]) - 96) * prev
+            prev*=power
+        ans = 0
+        if cur % modulo == hashValue:
+            ans = l - k
+        
+        newPow = power ** (k-1)
+        for i in range(l-k-1,-1,-1):
+            cur -= (ord(s[i+k]) - 96) * newPow
+            cur*=power
+            cur+=(ord(s[i]) - 96)
+            if cur % modulo == hashValue:
+                ans = i
+        return s[ans:ans+k]
+        
         
             
