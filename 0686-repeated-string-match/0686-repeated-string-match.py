ec1c30a0585a91d72d02db72b8189eed2189d724
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-10-13 08:12:14 +0300

    Time: 53 ms (26.30%), Space: 16.7 MB (10.67%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.py
+++ 0686-repeated-string-match/0686-repeated-string-match.py
@@ -1,17 +1,40 @@
 class Solution:
-    def repeatedStringMatch(self, a: str, b: str) -> int:
-        if b == "":
-            return 0
-        if a == b or b in a:
-            return 1
-        lenb = len(b)
-        lena = len(a)
-        c = 0
-        newA = a
-        while len(newA) < max(lena, lenb)*2:
-            newA +=a
-            c+=1
-            if b in newA:
-                return c+1
+    def repeatedStringMatch(self, a: str, b: str) -> int:            
+        lps = [0] * len(b)
+        prev, i = 0, 1
+        
+        while i < len(b):
+            if b[prev] == b[i]:
+                lps[i] = prev + 1
+                prev+=1
+                i+=1    
+            else:
+                if prev == 0:
+                    i+=1
+                else:
+                    prev = lps[prev-1]
+        bPtr = 0
+        tPtr = 0
+        count = 0
+        
+        while tPtr < len(a):
+            if tPtr == 0:
+                count+=1
+            if a[tPtr] == b[bPtr]:
+                tPtr+=1
+                tPtr%=len(a)
+                bPtr+=1
+            else:
+                if bPtr == 0:
+                    tPtr+=1
+                elif count > 2:
+                    return -1
+                else:
+                    bPtr = lps[bPtr-1]
+            if bPtr == len(b):
+                return count
+                
+        # print(tPtr)/
         return -1
-            
+        
+        
