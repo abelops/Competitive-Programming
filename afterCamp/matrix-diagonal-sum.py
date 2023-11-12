47cc1370258bf706f908338a609079ce35d82ffd
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-11-12 22:44:16 +0300

    Added solution for Matrix Diagonal Sum

---
+++ afterCamp/matrix-diagonal-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        ans = 0
+        for i in range(len(mat)):
+            ans+=mat[i][i]
+            if [i,i] != [i,len(mat)-i-1]:
+                ans+=mat[i][len(mat)-i-1]
+        return ans
+                
