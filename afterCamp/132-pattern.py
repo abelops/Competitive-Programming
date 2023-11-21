ac9281b8cc1f826f57d318f708f022b07f345054
Abel Getahun <88226208+abelops@users.noreply.github.com>
2023-11-21 21:59:14 +0300

    Added solution for 132 Pattern

--- afterCamp/132-pattern.py
+++ afterCamp/132-pattern.py
@@ -1,21 +1,18 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        mins = []
-        for i in range(len(nums)):
-            if i == 0:
-                mins.append(i)
-            else:
-                if nums[i] < nums[mins[-1]]:
-                    mins.append(i)
-                else:
-                    mins.append(mins[-1])
+        mins = [nums[0]]
+        for i in range(1, len(nums)):
+            mins.append(min(nums[i], mins[-1]))
         st = []
         for i in range(len(nums)):
-            while st and nums[st[-1]] <= nums[i]:
-                st.pop()
-            if st:
-                if nums[mins[st[-1]]] < nums[i]:
+            if not st:
+                st.append(i)
+            else:
+                while st and nums[st[-1]] <= nums[i]:
+                    st.pop()
+                # if st:
+                #     print(st, mins[st[-1]], nums[i])
+                if st and mins[st[-1]] < nums[i]:
                     return True
-            st.append(i)
+                st.append(i)
         return False
-                
